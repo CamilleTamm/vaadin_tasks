75766c4f3e015a6a1e3c40d29c4fb654d8b4d456
camille.tamim <camille.tamim0voip-telecom.com>
2021-11-22 10:46:55 +0100

    first commit

---
+++ src/main/java/model/Task.java
@@ -0,0 +1,10 @@
+package model;
+
+public class Task {
+
+    public String title;
+    public String date;
+    public String Status;
+    public String priority;
+    public int progress;
+}
---
+++ src/main/java/org/vaadin/example/MainView.java
@@ -0,0 +1,15 @@
+package org.vaadin.example;
+
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+
+@Route("")
+public class MainView extends VerticalLayout {
+
+    public MainView() {
+        add(
+                new H1("Vaadin Todo")
+        );
+    }
+}
